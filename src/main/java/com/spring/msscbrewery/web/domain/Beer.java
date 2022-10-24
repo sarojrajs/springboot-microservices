ba751cd65938966d52d1fb5c781286b8b89c9c4a
sarojrajs <54507969+sarojrajs@users.noreply.github.com>
2022-10-24 18:50:20 +0530

    jpa (#3)
    Co-authored-by: Saroj Raj Samantaray <sarojraj.s@engro.io>

---
+++ src/main/java/com/spring/msscbrewery/web/domain/Beer.java
@@ -0,0 +1,40 @@
+package com.spring.msscbrewery.web.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Beer {
+    @Id
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
